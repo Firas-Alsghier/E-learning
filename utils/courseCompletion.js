19fd42ba0290caf9b212c8f5f9a14ff9c0986907
Firas Alsghier <ferasalarape10@gmail.com>
2026-07-20 20:27:02 +0200

    Fixing & adding new features

---
+++ utils/courseCompletion.js
@@ -0,0 +1,7 @@
+import Course from '../models/Course.js';
+import Section from '../models/Section.js';
+import UserProgress from '../models/UserProgress.js';
+
+export async function getCourseCompletion(userId, courseId) {
+  // we'll write the logic next
+}
